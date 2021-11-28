b1334ecc7071db5ea7f04022def03b94c8a47ad9
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2021-11-28 20:29:11 +0200

    hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+    startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+let timerId = null;
 
+refs.startButton.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.startButton.disabled = true;
+});
+
+refs.stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.startButton.disabled = false;
+});
+
+//для генерации случайного цвета
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
