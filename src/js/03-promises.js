8fbb879ca4efbaec72bcb20675267659bf0c7ac8
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2021-11-30 23:46:30 +0200

    hw-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    form: document.querySelector('.form'),
+    delay: document.querySelector('[name="delay"]'),
+    step: document.querySelector('[name="step"]'),
+    amount: document.querySelector('[name="amount"]'),
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay);
+    });
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    let promiseDelay = Number(refs.delay.value);
+    const stepValue = Number(refs.step.value);
+    const amountValue = Number(refs.amount.value);
+
+    for (let i = 1; i <= amountValue; i += 1) {
+        createPromise(i, promiseDelay).then(({ position, delay }) => {
+                Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+
+        promiseDelay += stepValue;
+    }
 }
